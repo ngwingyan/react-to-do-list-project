39bc072d389985572a90932a6983c89ef23159e6
webdevtonym <75320313+webdevtonym@users.noreply.github.com>
2023-03-21 18:35:08 +0000

    finished inspirational quote component and pushing to branch

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./App.css";
 
 import TodoList from "./components/TodoList";
+import InspirationalQuote from "./components/InspirationalQuote";
+
 
 function App() {
   return (
     <div className="todo-app">
       <TodoList />
+      <InspirationalQuote className="test"/>
     </div>
   );
 }
