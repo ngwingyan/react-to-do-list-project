4753efb5245d60731a657a3ca04bcce17b2206f3
paulpaulchan <116852901+ngwingyan@users.noreply.github.com>
2023-03-23 05:40:54 +0000

    update current date API

---
+++ src/components/Time.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const TimeDisplay = () => {
+  const [dateData, setDateData] = useState(null);
+
+  useEffect(() => {
+    fetchDateData();
+  }, []);
+
+  const fetchDateData = async () => {
+    try {
+      const response = await axios.get('http://worldtimeapi.org/api/ip');
+      const options = { year: 'numeric', month: 'short', day: 'numeric' };
+      const date = new Date(response.data.datetime);
+      // no set format, according to your IP
+      const formattedDate = date.toLocaleDateString('en-uk', options);
+      setDateData(formattedDate);
+    } catch (error) {
+      console.log("Error fetching quote:", error);
+    }
+  };
+  return (
+    <div className="currentDate">
+      {dateData}
+    </div>
+  )
+};
+
+export default TimeDisplay;
