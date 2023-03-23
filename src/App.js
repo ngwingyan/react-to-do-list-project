f6c7b0f2bb5df23a35c98f8b293d94d91e559e7e
paulpaulchan <116852901+ngwingyan@users.noreply.github.com>
2023-03-23 19:21:58 +0000

    incorporate drag and drop

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "./App.css";
-import DateDisplay from "./components/Date";
 import TodoList from "./components/TodoList";
+import DateDisplay from "./components/Date"
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import InspirationalQuote from "./components/InspirationalQuote";
 
-function App() {
+function App () {
   return (
+    <DndProvider backend={HTML5Backend}>
     <div className="todo-app">
       <InspirationalQuote className="quote" />
       <h1>Plans for Today <DateDisplay /> </h1>
       <TodoList />
     </div>
+    </DndProvider>
   );
 }
 
