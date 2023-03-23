ca79ee4fae23560addc305303cd46fb0b754a627
paulpaulchan <116852901+ngwingyan@users.noreply.github.com>
2023-03-23 21:54:41 +0000

    edit the http to https

--- src/components/Date.js
+++ src/components/Date.js
@@ -10,7 +10,7 @@ const DateDisplay = () => {
 
   const fetchDateData = async () => {
     try {
-      const response = await axios.get("http://worldtimeapi.org/api/ip");
+      const response = await axios.get("https://worldtimeapi.org/api/ip");
       const options = { year: "numeric", month: "short", day: "numeric" };
       const date = new Date(response.data.datetime);
       const formattedDate = date.toLocaleDateString("en-uk", options);
